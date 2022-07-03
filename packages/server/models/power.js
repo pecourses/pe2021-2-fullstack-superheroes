ad21c9841d66275387ba7d6758ce392a1fc36805
Viktoriia <pecourses>
2022-07-03 17:28:54 +0300

    Add powers model + migration

---
+++ packages/server/models/power.js
@@ -0,0 +1,23 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Power extends Model {
+    static associate (models) {}
+  }
+  Power.init(
+    {
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      fullDescription: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'Power',
+      underscored: true,
+    }
+  );
+  return Power;
+};
