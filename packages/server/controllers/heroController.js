a5df3742fe2b436b99d6f7bd1018f5406f7983be
Viktoriia <pecourses>
2022-07-03 19:55:47 +0300

    Add createHero controller

--- packages/server/controllers/heroController.js
+++ packages/server/controllers/heroController.js
@@ -1,5 +1,26 @@
+const { Hero } = require('./../models');
+
 module.exports.createHero = async (req, res, next) => {
-  res.status(501).send('Not Implemented');
+  const { body, file } = req;
+
+  body.image = file.filename;
+  const powers = body.superpowers
+    .split('')
+    .filter(s => s !== '[' && s !== ']')
+    .join('')
+    .split(',')
+    .map(i => Number(i));
+
+  try {
+    // добавить основную инфу в супергерои
+    const createdHero = await Hero.create(body);
+    // добавить инфу о суперсилах созданного героя в таблицу связи
+    const createdHeroPowers = await createdHero.setPowers(powers);
+    // console.log('createdHeroPowers', createdHeroPowers);
+    rss.stsatus(501).send('Not Implemented');
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports.getHeroes = async (req, res, next) => {
