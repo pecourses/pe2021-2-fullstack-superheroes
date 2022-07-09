4a5d77f3ab4fc15909b933559dc0185f43d4d356
Viktoriia <pecourses>
2022-07-09 19:38:17 +0300

    Add HeroesList

--- packages/client/src/components/HeroesList/index.jsx
+++ packages/client/src/components/HeroesList/index.jsx
@@ -1,7 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const axiosInstance = axios.create({ baseURL: 'http://localhost:5000/api' });
 
 function HeroesList () {
-  return <div>HeroesList</div>;
+  const [heroes, setHeroes] = useState([]);
+  const [powers, setPowers] = useState([]);
+  // подгрузить силы
+
+  useEffect(() => {
+    axiosInstance
+      .get('/heroes')
+      .then(response => {
+        setHeroes(response.data.data);
+      })
+      .catch(err => console.log('err', err));
+
+    axiosInstance
+      .get('/powers')
+      .then(response => {
+        setPowers(response.data.data);
+      })
+      .catch(err => console.log('err', err));
+  }, []);
+
+  const mapHeroes = ({
+    id,
+    image,
+    nickname,
+    originDescription,
+    catchPhrase,
+    realName,
+    superpowers,
+    isGood,
+  }) => (
+    <li key={id}>
+      <input type='checkbox' checked={isGood} onChange={() => {}} />
+      <img
+        style={{ width: '100px', height: '100px', objectFit: 'cover' }}
+        src={
+          image
+            ? `http://localhost:5000/images/${image}`
+            : './defaultHeroImage.png'
+        }
+        alt={nickname}
+      />
+      {nickname} {realName}
+      <p>{originDescription}</p>
+      <p>{catchPhrase}</p>
+      <ol>
+        {superpowers.map(p => (
+          <li>{powers[powers.findIndex(i => p === i.id)].description}</li>
+        ))}
+      </ol>
+    </li>
+  );
+
+  return <ul>{heroes.map(mapHeroes)}</ul>;
 }
 
 export default HeroesList;
